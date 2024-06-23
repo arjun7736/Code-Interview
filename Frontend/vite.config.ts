01bcc61ccc49f47d9f4e284bbac1ad7f497f7b4c
arjun7736 <arjunvengassery123@gmail.com>
2024-06-23 13:50:18 +0530

    fix: interviewer login

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -4,25 +4,15 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     '/api': {
-  //       target: 'http://13.233.85.129/',
-  //       changeOrigin:true,
-  //       secure: false,
-  //     }
-  //   }
-  // },
   server: {
     proxy: {
       '/api': {
-        target: 'http://codeinterview.s3-website.ap-south-1.amazonaws.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        target: 'http://13.233.85.129/',
+        changeOrigin:true,
+        secure: false,
       }
     }
   },
-  
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
