bb4d73c3b9bdc4780b54474e495333c13f746a39
arjun7736 <arjunvengassery123@gmail.com>
2024-06-23 13:45:28 +0530

    chnaged config

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -4,15 +4,25 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://13.233.85.129/',
+  //       changeOrigin:true,
+  //       secure: false,
+  //     }
+  //   }
+  // },
   server: {
     proxy: {
       '/api': {
-        target: 'http://13.233.85.129/',
-        changeOrigin:true,
-        secure: false,
+        target: 'http://codeinterview.s3-website.ap-south-1.amazonaws.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
       }
     }
   },
+  
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
