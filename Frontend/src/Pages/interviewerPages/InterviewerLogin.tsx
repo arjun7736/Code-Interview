fa429f57ef8715cee27903d758e20cd51987deed
Arjun v <56952204+arjun7736@users.noreply.github.com>
2024-06-27 11:47:33 +0530

    Update InterviewerLogin.tsx

--- Frontend/src/Pages/interviewerPages/InterviewerLogin.tsx
+++ Frontend/src/Pages/interviewerPages/InterviewerLogin.tsx
@@ -33,7 +33,7 @@ const InterviewerLogin = () => {
     dispatch(loginStart());
 
     try {
-     await axios.post("http://13.233.229.71/api/auth/login", { ...formData, role: "interviewer" },{ withCredentials: true }
+     await axios.post("http://13.233.229.71/api/auth/login", { ...formData, role: "interviewer" }
      ).then((data)=>{
        dispatch(loginSuccess(data.data));
        navigate("/interviewer");
