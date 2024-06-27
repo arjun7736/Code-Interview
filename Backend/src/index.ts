e255c53caa5c1d7817afbb02d01d9eaa3bb52f84
Arjun v <56952204+arjun7736@users.noreply.github.com>
2024-06-27 11:12:49 +0530

    Update index.ts

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -9,7 +9,7 @@ import cors from "cors"
 const URI: string | undefined = process.env.MONGO_URI;
 const PORT: string | undefined = process.env.PORT;
 const corsOptions = {
-  origin: 'http://codeinterview.s3-website.ap-south-1.amazonaws.com',
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
